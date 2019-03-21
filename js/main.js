eec551b5316e2ad7459ef5074eb1e9e3eada1d2c
Josephchinedu <47852925+Josephchinedu@users.noreply.github.com>
2019-03-21 16:12:08 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,137 @@
+$(document).ready(function() {
+  //------- Pre Loader --------//
+  $(window).on("load", function() {
+    $(".preloader-area")
+      .delay(200)
+      .fadeOut(500);
+  });
+
+
+
+  "use strict";
+
+  var window_width = $(window).width(),
+    window_height = window.innerHeight,
+    header_height = $(".default-header").height(),
+    header_height_static = $(".site-header.static").outerHeight(),
+    fitscreen = window_height - header_height;
+
+  //   $(".fullscreen").css("height", window_height);
+  $(".fitscreen").css("height", fitscreen);
+
+  //------- Wow JS Initialized --------//
+  new WOW().init();
+
+  //------- Go to Top --------//
+  $(window).on("scroll", function() {
+    if ($(this).scrollTop() > 100) {
+      $("#header").addClass("header-scrolled");
+      $("#back-top").addClass("back-top-animation");
+    } else {
+      $("#header").removeClass("header-scrolled");
+      $("#back-top").removeClass("back-top-animation");
+    }
+  });
+
+
+
+
+  //------- Mobile Nav  js --------//
+
+  if ($("#nav-menu-container").length) {
+    var $mobile_nav = $("#nav-menu-container")
+      .clone()
+      .prop({
+        id: "mobile-nav"
+      });
+    $mobile_nav.find("> ul").attr({
+      class: "",
+      id: ""
+    });
+    $("body").append($mobile_nav);
+    $("body").prepend(
+      '<button type="button" id="mobile-nav-toggle"><i class="ti-menu"></i></button>'
+    );
+    $("body").append('<div id="mobile-body-overly"></div>');
+    $("#mobile-nav")
+      .find(".menu-has-children")
+      .prepend('<i class="ti-angle-down"></i>');
+
+    $(document).on("click", ".menu-has-children i", function(e) {
+      $(this)
+        .next()
+        .toggleClass("menu-item-active");
+      $(this)
+        .nextAll("ul")
+        .eq(0)
+        .slideToggle();
+      $(this).toggleClass("ti-angle-up ti-angle-down");
+    });
+
+    $(document).on("click", "#mobile-nav-toggle", function(e) {
+      $("body").toggleClass("mobile-nav-active");
+      $("#mobile-nav-toggle i").toggleClass("ti-close ti-menu");
+      $("#mobile-body-overly").toggle();
+    });
+
+    $(document).click(function(e) {
+      var container = $("#mobile-nav, #mobile-nav-toggle");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($("body").hasClass("mobile-nav-active")) {
+          $("body").removeClass("mobile-nav-active");
+          $("#mobile-nav-toggle i").toggleClass("ti-close ti-menu");
+          $("#mobile-body-overly").fadeOut();
+        }
+      }
+    });
+  } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
+    $("#mobile-nav, #mobile-nav-toggle").hide();
+  }
+
+  //------- Smooth Scroll  js --------//
+
+  $(".nav-menu a, #mobile-nav a, .scrollto").on("click", function() {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      if (target.length) {
+        var top_space = 0;
+
+        if ($("#header").length) {
+          top_space = $("#header").outerHeight();
+
+          if (!$("#header").hasClass("header-fixed")) {
+            top_space = top_space;
+          }
+        }
+
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - top_space
+          },
+          1500,
+          "easeInOutExpo"
+        );
+
+        if ($(this).parents(".nav-menu").length) {
+          $(".nav-menu .menu-active").removeClass("menu-active");
+          $(this)
+            .closest("li")
+            .addClass("menu-active");
+        }
+
+        if ($("body").hasClass("mobile-nav-active")) {
+          $("body").removeClass("mobile-nav-active");
+          $("#mobile-nav-toggle i").toggleClass("ti-menu ti-menu");
+          $("#mobile-body-overly").fadeOut();
+        }
+        return false;
+      }
+    }
+  });
+
+
+})
